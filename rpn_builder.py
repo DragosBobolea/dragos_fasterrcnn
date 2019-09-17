6ffa284464b3ebfddd8a77229a53e55af483a449
Dragos Bobolea <boboleadragos@gmail.com>
2019-09-17 13:41:50 +0300

    Ported anchor generation in TF 2.0

--- rpn_builder.py
+++ rpn_builder.py
@@ -14,15 +14,14 @@ class RegionProposalNetwork(keras.Model):
         self.batch_size = 256
         self.positives_ratio = 0.5
         self.max_number_of_predictions = 400
+
         # parameters
         self.backbone = backbone
         self.scales = scales
         self.ratios = ratios
+        self.anchor_templates = self.__get_anchor_templates()
 
         # layers
-        self.image = tf.placeholder(dtype=float, shape=(None, None, None, 3), name='image')
-        self.ground_truth_boxes = tf.placeholder(dtype=float, shape=(None, None, 4), name='ground_truth_boxes')
-        
         self.conv1 = keras.layers.Conv2D(filters=256,kernel_size=3,activation='relu')
         self.box_regression = keras.layers.Conv2D(4, 1)
         self.box_classification = keras.layers.Conv2D(1,1,activation='softmax')
@@ -47,6 +46,56 @@ class RegionProposalNetwork(keras.Model):
         anchors = generate_anchors(image.shape)
         anchors_batch_indices, positive_anchors_indices, negative_anchors_indices = generate_minibatch_mask(anchors, ground_truths)
     
-    
+
+    '''
+    Generates anchor templates, in XYXY format, centered at 0
+    Returns:
+        anchor_templates: numpy array of shape (n_scales * n_ratios, 4)
+    '''
+    def __get_anchor_templates(self):
+        anchor_templates = np.zeros((len(self.ratios) * len(self.scales), 4))
+        for ratio_idx in range(len(self.ratios)):
+            for scale_idx in range(len(self.scales)):
+                ratio = self.ratios[ratio_idx]
+                scale = self.scales[scale_idx]
+                area = scale * self.base_anchor_size ** 2
+                width = np.sqrt(ratio * area)
+                height = area / width
+                anchor = [-width/2, -height/2, width/2, height/2]
+                anchor_templates[ratio_idx * len(self.scales) + scale_idx] = anchor
+
+        return anchor_templates.astype(np.float32)
+
+    '''
+    Generates anchors for feature map
+    Anchors are in XYXY format, in absolute pixel values
+    Args:
+        feature_map: tensor of shape (1, height, width, channels)
+    Returns:
+        anchors: tensor of shape (1, height, width, num_anchors, 4)
+    '''
+    @tf.function
+    def generate_anchors(self, feature_map):
+        # TODO support minibatch by tiling anchors on first dimension
+        assert feature_map.shape[0] == 1
+        vertical_stride = tf.range(0,feature_map.shape[1])
+        vertical_stride = tf.tile(vertical_stride,[feature_map.shape[2]])
+        vertical_stride = tf.reshape(vertical_stride, (feature_map.shape[2], feature_map.shape[1]))
+        vertical_stride = tf.transpose(vertical_stride)
+
+        horizontal_stride = tf.range(0,feature_map.shape[2])
+        horizontal_stride = tf.tile(horizontal_stride, [feature_map.shape[1]])
+        horizontal_stride = tf.reshape(horizontal_stride, (feature_map.shape[1], feature_map.shape[2]))
+
+        centers_xyxy = tf.stack([horizontal_stride, vertical_stride, horizontal_stride, vertical_stride], axis=2)
+
+        centers_xyxy = self.stride * centers_xyxy
+        centers_xyxy = tf.cast(centers_xyxy,tf.float32)
+
+        centers_xyxy = tf.tile(centers_xyxy,[1,1,self.anchor_templates.shape[0]])
+        centers_xyxy = tf.reshape(centers_xyxy, (feature_map.shape[1], feature_map.shape[2], self.anchor_templates.shape[0], 4))
+        anchors = centers_xyxy + self.anchor_templates
+        anchors = tf.expand_dims(anchors,axis=0)
+        return anchors
 
 
--- rpn_tests.py
+++ rpn_tests.py
@@ -1,25 +1,44 @@
 import os
 import numpy as np
-from anchors import generate_anchors, classify_anchors, generate_minibatch
-from rpn_builder import build_rpn
+from rpn_builder import RegionProposalNetwork
 import unittest
 import cv2
 import tensorflow as tf
 keras = tf.keras
 
 class RpnTest(unittest.TestCase):
-    def test_rpn(self):
+    def test_anchors(self):
         DEBUG = True
+        if DEBUG:
+            scales = [0.5, 1, 2]
+            ratios = [0.5, 1, 2]
+            image = cv2.imread(os.path.join('images','1.jpg'))
+            image_tensor = image.astype(np.float32) \
+                .reshape((1,image.shape[0],image.shape[1],3))
+
+            backbone = None
+            rpn = RegionProposalNetwork(backbone, scales, ratios)
+            image_feature_map = np.zeros((1, image.shape[0] // rpn.stride, image.shape[1] // rpn.stride, 2048))
+            anchors = rpn.generate_anchors(image_feature_map)
+            anchors = np.array(anchors).reshape(-1,4)
+            for anchor in anchors:
+                cv2.rectangle(image, (int(anchor[0]), int(anchor[1])), (int(anchor[2]), int(anchor[3])), (0,0,255),1)
+            cv2.imshow('anchors', image)
+            cv2.waitKey(0)
+            
+    def test_rpn(self):
+        DEBUG = False
         if DEBUG:
             scales = [1]
             ratios = [1]
             image = cv2.imread(os.path.join('images','1.jpg'))
+            image = image.astype(np.float32) \
+                .reshape((1,image.shape[0],image.shape[1],3))
 
-            image_shape = (512, 512, 3)
-            input_tensor = tf.placeholder(dtype=float, shape=(None, 512, 512, 3))
-            base_model = keras.applications.ResNet50(input_tensor=input_tensor, include_top=False)
-            base_model.trainable = False
-            rpn = build_rpn(base_model)
-        
+            backbone = keras.applications.ResNet50(include_top=False)
+            rpn = RegionProposalNetwork(backbone, scales, ratios)
+            image_feature_map = backbone(image)
+            anchors = rpn.generate_anchors(image_feature_map)
+            print(anchors)
 if __name__ == '__main__':
     unittest.main()
